c3e599912ec1cf7e331cabe69a3b690ac1f089ba
cardizaDev <cardizaDEV@gmail.com>
2026-03-17 02:42:05 +0100

    Project gen

---
+++ src/main/resources/db/migration/V1__portfolio_base.sql
@@ -0,0 +1,46 @@
+CREATE TABLE organization (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    icon_base64 TEXT
+);
+
+CREATE TABLE technology (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    icon_base64 TEXT
+);
+
+CREATE TABLE experience_status (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE experience_category (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE experience (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    organization_id INTEGER REFERENCES organization(id) NOT NULL,
+    status_id INTEGER REFERENCES experience_status(id) NOT NULL,
+    category_id INTEGER REFERENCES experience_category(id) NOT NULL,
+    description TEXT,
+    start_date DATE NOT NULL,
+    end_date DATE
+);
+
+CREATE TABLE comment (
+    id SERIAL PRIMARY KEY,
+    user_id VARCHAR(100) NOT NULL,
+    experience_id INTEGER REFERENCES experience(id) NULL,
+    comment_text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE experience_technology (
+    experience_id INTEGER REFERENCES experience(id) NOT NULL,
+    technology_id INTEGER REFERENCES technology(id) NOT NULL,
+    PRIMARY KEY (experience_id, technology_id)
+);
